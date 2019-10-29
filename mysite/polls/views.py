1a085ef5dce91f9a544c8e181da179068f773993
Uğur Aldanmaz <ugur.aldanmaz@outlook.com>
2019-10-29 22:47:59 +0300

    add first template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,16 +5,20 @@ from .models import Question
 
 # Create your views here.
 
+
 def index(request):
-    latest_question = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_question)
-    return HttpResponse(output)
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse('This is the detail view of the question: %s' % question_id)
 
+
 def results(request, question_id):
     return HttpResponse('These are the result of the question: %s' % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse('Vote on question: %s' % question_id)
